1d44e0d57d872e302ce842134acbec30aa38d3b3
Oleksandra Kirina <111364619+Sashakirina@users.noreply.github.com>
2024-07-07 12:59:41 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import Contact from "../Contact/Contact";
+import style from "./ContactList.module.css";
+import { selectFilter } from "../../redux/filtersSlice";
+
+function ContactList() {
+	const contacts = useSelector(selectContacts);
+	const filter = useSelector(selectFilter);
+	console.log(contacts);
+
+	const visibleContacts = () => {
+		return contacts.filter(({ name }) =>
+			name.toLowerCase().includes(filter.toLowerCase())
+		);
+	};
+
+	return (
+		<ul className={style.phonebookContainer}>
+			{visibleContacts().map(({ id, name, number }) => (
+				<Contact key={id} name={name} number={number} id={id} />
+			))}
+		</ul>
+	);
+}
+
+export default ContactList;
